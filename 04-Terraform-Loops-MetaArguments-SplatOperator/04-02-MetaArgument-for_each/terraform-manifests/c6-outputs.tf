d4dec159367538000596da8209f03c94fc1892dd
SachinPitale <sachinpitale22@gmail.com>
2022-11-02 12:55:31 +0530

    updated output tf

--- 04-Terraform-Loops-MetaArguments-SplatOperator/04-02-MetaArgument-for_each/terraform-manifests/c6-outputs.tf
+++ 04-Terraform-Loops-MetaArguments-SplatOperator/04-02-MetaArgument-for_each/terraform-manifests/c6-outputs.tf
@@ -3,18 +3,18 @@
 # EC2 Instance Public IP with TOSET
 output "instance_publicip" {
   description = "EC2 Instance Public IP"
-  value = [for instance in aws_instance.myec2: instance.public_ip]
+  value = toset([for instance in aws_instance.myec2: instance.public_ip])
   
 }
 
 output "instance_publicdns" {
   description = "EC2 Instance Public DNS"
-  value = [for instance in aws_instance.myec2: instance.public_dns]
+  value = toset([for instance in aws_instance.myec2: instance.public_dns])
   
 }
 
 output "instance_publicdns2" {
   description = "EC2 Instance Public DNS"
-  value = {for c, instance in aws_instance.myec2: c => instance.public_dns}
+  value = toset({for c, instance in aws_instance.myec2: c => instance.public_dns})
   
 }
