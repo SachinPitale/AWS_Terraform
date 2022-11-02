7e901c17ac48ae487029b7771c79a9c99fa4baec
SachinPitale <sachinpitale22@gmail.com>
2022-11-02 11:52:46 +0530

    update output

--- 04-Terraform-Loops-MetaArguments-SplatOperator/04-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c5-ec2instance.tf
+++ 04-Terraform-Loops-MetaArguments-SplatOperator/04-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c5-ec2instance.tf
@@ -2,8 +2,8 @@
 resource "aws_instance" "myec2" {
   ami = data.aws_ami.amzlinux2.id
   #instance_type = var.instance_type
-  #instance_type = var.instance_type_list[0] # reference List values
-  instance_type = var.instance_type_map["dev"] #reference Map values
+  instance_type = var.instance_type_list[0] # reference List values
+  #instance_type = var.instance_type_map["dev"] #reference Map values
   key_name = var.instance_keypair
   user_data = file("${path.module}/app1-install.sh")
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
